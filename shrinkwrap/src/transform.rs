9fe278bcb90a15f17d32a80c1f08d8105ffac428
Kyle King <kyle.king@shaw.ca>
2025-08-05 21:41:30 -0700

    Update docs

--- shrinkwrap/src/transform.rs
+++ shrinkwrap/src/transform.rs
@@ -69,7 +69,7 @@ pub trait Transform {
 /// ### Deeply Nested
 ///
 /// If the nest is layered under some other nest (deeply nested), the `impl` has a similar structure to the standard impl.
-/// The only real change is instead of using the primary data source (`MyData`) for the first generic parameter, you would use the parent nest.
+/// The only real change is instead of using the primary data source (`MyData`) as the associated data type, you would use the parent nest.
 ///
 /// This example assumes two nests, a top-level/standard nest `usd_value`, and a deeply nested `text` under `usd_value`
 ///
@@ -105,22 +105,17 @@ pub trait Transform {
 ///
 /// ## Notes
 ///
-/// When a nest (e.g. `NestA`) has children nests layered under it (deeply nested), it's type will be swapped out with a wrapper (`NestAWrapper`) within `NestA`'s parent.
+/// When a nest has child nests layered under it (deeply nested), it's type will be swapped out with a dedicated 'injected' wrapper.
 ///
 /// However, this does not affect the trait impls above - the `Wrap` derive macro automatically adds an implementation for the wrapper->nest translation.
 ///
-/// This is the reason that the [`TransformToNest`] impls for deeply nested structs requires so few adjustments and why there is no impl over `NestAWrapper`
-/// - it's already done for you). You just need to implement the mapping of `NestA` -> `TheChildNestB` - which is exactly what is shown above.
-///
+/// The only requirement is that `TransformToNest` is implemented from the data source to the nest type.
 pub trait TransformToNest<N>: Transform {
     type Data;
     fn transform_to_nest(&self, data: &Self::Data, options: &Self::Options) -> N;
 }
 
-/// Not intended for external implementation.
-///
-/// Users should instead implement [`TransformToNest`]. This trait is interoperable and can be implemented on any struct, not just Data structs.
-/// This allows for dependency injection and/or state management.
+/// Not intended for implementation by consumers. Users should instead implement [`TransformToNest`].
 ///
 /// [`ToNestWith`] will be implemented automatically when [`TransformToNest`] is implemented on the corresponding types.
 pub trait ToNestWith<N, T: Transform>: Sized
@@ -130,7 +125,7 @@ where
     fn to_nest_with(&self, transform: &T, options: &T::Options) -> N;
 
 }
-/// Blanket implementation providing `to_nest_with(transform)` for data structs that have a corresponding transform impl (`impl TransformToNest<Data, Nest> for MyTransform`)
+/// Blanket implementation providing `to_nest_with(transform)` for data structs that have a corresponding transform impl (`impl TransformToNest<Nest> for MyTransform`)
 impl<D, N, T> ToNestWith<N, T> for D
 where
     T: TransformToNest<N, Data = D>
@@ -140,7 +135,7 @@ where
     }
 }
 
-/// `ToWrappedWith` is automatically implemented when all top-level nests have a [`TransformToNest`] impl.
+/// `ToWrappedWith` is automatically implemented when all top-level nests have a [`TransformToNest`] impl on each nest type within the group. The same transform type  is implied (and cannot be configured anyhow).
 ///
 /// Furthermore, any nests which are deeply nested require a [`TransformToNest`] converting from their respective data source (the parent nest).
 pub trait ToWrappedWith<T>: Debug + Clone + Serialize
@@ -154,7 +149,7 @@ where
 
 /// Automatically implemented across types that provide `ToWrappedWith`.
 ///
-/// This allows for converting data from the desired wrapper type.
+/// This allows for converting data via the Wrapper type instead
 pub trait WrapDataWith<D, T>
 where
     T: Transform,
