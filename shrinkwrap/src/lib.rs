aa6252b33c2e416c10f13f31ebead6e0d73fd2ca
Kyle King <git@kylek.ing>
2026-04-22 19:59:46 -0700

    Add new library types / traits for auto-derive

--- shrinkwrap/src/lib.rs
+++ shrinkwrap/src/lib.rs
@@ -12,3 +12,19 @@ pub use crate::{
     wrap::{ToWrappedWith, WrapDataWith},
 };
 pub use shrinkwrap_macros::Wrap;
+
+
+pub trait NestValueType {}
+
+pub trait NestGroup {
+    type Value: NestValueType;
+}
+
+pub trait BuildNestValue<T, NV: NestValueType>: Transform {
+    fn build_nest_value(&self, source: &T, options: &Self::Options) -> NV;
+}
+
+pub trait TryBuildNestValue<T, NV: NestValueType>: Transform {
+    type Error;
+    fn try_build_nest_value(&self, source: &T, options: &Self::Options) -> Result<NV, Self::Error>;
+}
